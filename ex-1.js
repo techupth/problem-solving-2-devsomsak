e09d04d4dad7f6c9500e6ef502bd5105c1bc69e2
devsomsak <devrleang@gmail.com>
2023-12-23 13:43:54 +0700

    feat:solve RomanToInt

--- ex-1.js
+++ ex-1.js
@@ -1,9 +1,30 @@
-//Exercise 1: Roman to Integer
+const romanToInt = function (stringRomans) {
+  const romanNumerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
 
-const romanToInt = function (s) {
-  //Start coding here
+  let total = 0;
+  let prevNumber = 0;
+
+  for (let i = 0; i < stringRomans.length; i++) {
+    const currentNumber = romanNumerals[stringRomans[i]];
+
+    if (prevNumber === 0) {
+      total = currentNumber;
+    } else if (currentNumber > prevNumber) {
+      total = total + currentNumber - (2 * prevNumber);
+    } else {
+      total = total + currentNumber;
+    }
+      prevNumber = currentNumber;
+  }
+  return total;
 };
-
-const result1 = romanToInt("III"); // 3
-const result2 = romanToInt("LVIII"); // 58
-const result3 = romanToInt("MCMXCIV"); //1994
+const romanNumber = "IV";
+console.log(romanToInt(romanNumber)); // Output: 6
